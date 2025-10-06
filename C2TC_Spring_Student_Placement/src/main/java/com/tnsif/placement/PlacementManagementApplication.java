1f3a10a56ec5e92ad358551db93e7ea66b31b96d
Lenovo <Lenovo@DESKTOP-TCIFSAN>
2025-10-06 15:18:38 +0530

    Initial Commit

--- C2TC_Spring_Student_Placement/src/main/java/com/tnsif/placement/PlacementManagementApplication.java
+++ C2TC_Spring_Student_Placement/src/main/java/com/tnsif/placement/PlacementManagementApplication.java
@@ -3,7 +3,7 @@ package com.tnsif.placement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.tsinf.placement", "com.tnsif.PlacementManagement_Student"})
 public class PlacementManagementApplication{   // ✅ Class name corrected
 
     public static void main(String[] args) {
